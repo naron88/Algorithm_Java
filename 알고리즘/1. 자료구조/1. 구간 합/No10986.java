0fd8f8068c1320b280596b834902af6c9a3c6bf2
ByeongGyu <93171052+naron88@users.noreply.github.com>
2025-01-15 09:44:25 +0900

    Update No10986.java

--- 알고리즘/1. 자료구조/1. 구간 합/No10986.java
+++ 알고리즘/1. 자료구조/1. 구간 합/No10986.java
@@ -2,32 +2,42 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        long arr[] = new long[N+1];
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // 수 N, M 입력받기
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
         long count = 0;
-        long remains[] = new long[M];
 
-        arr[0] = 0;
-        for (int i = 1; i <= N; i++) {
-            arr[i] = arr[i-1] + sc.nextLong();
-        }
+        // N개의 수 입력 받기
+        // 합 배열 만들기
+        long[] sumArr = new long[N+1];
+        long[] mCount = new long[M];
+        st = new StringTokenizer(br.readLine());
 
         for (int i = 1; i <= N; i++) {
-            int remain = (int)(arr[i] % M);
-            if (remain == 0) {
+            sumArr[i] = sumArr[i-1] + Long.parseLong(st.nextToken());
+
+            // 1. 구간 합 % M == 0 인 경우 구하기
+            if (sumArr[i] % M == 0){
                 count++;
             }
-            remains[remain]++;
+            mCount[(int)(sumArr[i] % M)]++;
         }
 
+        // 2. 구간합 A % M - B % M == 0 인 경우 구하기
+        //      nC2 = n * (n-1) / 2
         for (int i = 0; i < M; i++) {
-            if (remains[i] > 1){
-                count += remains[i] * (remains[i]-1) / 2;
+            if (mCount[i] > 1){
+                count += mCount[i] * (mCount[i] - 1) / 2;
             }
         }
-        System.out.println(count);
+
+        bw.write(count + "\n");
+        bw.flush();
+        bw.close();
     }
 }
