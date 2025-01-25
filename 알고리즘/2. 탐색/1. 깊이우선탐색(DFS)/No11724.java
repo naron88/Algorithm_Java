60626e91f900292c767243a5fba2888b93e6814f
ByeongGyu <93171052+naron88@users.noreply.github.com>
2025-01-25 22:37:41 +0900

    Update No11724.java

--- 알고리즘/2. 탐색/1. 깊이우선탐색(DFS)/No11724.java
+++ 알고리즘/2. 탐색/1. 깊이우선탐색(DFS)/No11724.java
@@ -1,59 +1,47 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class No11724 {
-    // 연결 오소 개수
-    // 11724
-    private static LinkedList<Integer>[] graph;
-    private static boolean[] visited;
+public class Main{
+    public static boolean[] visited;
+    public static Map<Integer, List<Integer>> graph;
+    public static int count;
 
-    // DFS 함수 구현
-    public static void DFS(int start){
-        // 이미 방문했을 경우 = 종료
-        if(visited[start]) {
+    public static void DFS(int u){
+        if (visited[u]){
             return;
         }
-
-        // 노드에 방문했으므로 true
-        visited[start] = true;
-
-        // 그래프에 있는 노드가 끝날 때까지 반복
-        for (int i : graph[start]){
-            if (!visited[i]){
-                DFS(i);
-            }
+        visited[u] = true;
+        for (int i : graph.get(u)){
+            DFS(i);
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        // n(노드 수), m(에지 수) 입력받기
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        graph = new LinkedList[n + 1];
-        visited = new boolean[n + 1];
-
-        for (int i = 1; i <= n; i++) {
-            graph[i] = new LinkedList<>();
+        // 정점의 개수 N, 간선의 개수 M 받기, 방문 리스트 생성, 요소 개수 생성
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N+1];
+        count = 0;
+        // M개의 양 끝점 u, v 입력 받기, 방문 리스트 초기화
+        graph = new HashMap<>();
+        for (int i = 1; i <= N; i++){
+            graph.put(i, new LinkedList<>());
+            visited[i] = false;
         }
-
-        for (int i = 0; i < m; i++) {
-            // 시작 노드, 끝 노드 입력 받기
+        for (int i = 0; i < M; i++){
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            graph[start].add(end);
-            graph[end].add(start);
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph.get(u).add(v);
+            graph.get(v).add(u);
         }
-        int count = 0;
-        for (int i = 1; i < n + 1; i++) {
-            if(!visited[i]){
+
+        // DFS
+        // N번 반복
+        for (int i = 1; i <= N; i++){
+            if (!visited[i]){
                 count++;
                 DFS(i);
             }
